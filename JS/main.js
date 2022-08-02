c83df07f1e07d98b3c7dcf7fd2a0682a30a5d3bd
Ignacio Sosa Federmann <ignaciososa62@gmail.com>
2022-08-02 13:59:04 -0300

    1er pre entrega del proyecto

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,97 @@
-
-
 let postre = prompt("Ingrese el plato que quiera comprar");
 
 let adicional = prompt("Ingrese de que sabor lo quiere");
 
-let Pago = prompt("Ingrese con que metodo va a pagar");
-
-
+let pago = prompt("Ingrese con que metodo va a pagar");
 
 while(!postre) {
+
     postre = prompt("Ingrese el plato que quiera comprar");
+
 }
 
 while(!adicional) {
+
     adicional = prompt("Ingrese de que sabor lo quiere");
+
 }
 
-while(!Pago) {
-    Pago = prompt("Ingrese con que metodo va a pagar");
-}
+while(!pago) {
 
+    pago = prompt("Ingrese con que metodo va a pagar");
+
+}
 
 let confirmacion = "¿Confirma que los datos ingresados son correctos?";
 
 confirmacion += "\n" + "Postre: " + postre;
+
 confirmacion += "\n" + "Adicional: " + adicional;
-confirmacion += "\n" + "Metodo de pago: " + Pago;
+
+confirmacion += "\n" + "Metodo de pago: " + pago;
 
 let respuesta = confirm(confirmacion);
 
 while (respuesta !="") {
+
     if(respuesta){
+
         alert ("Se ha tomado el pedido");
+        break
     }
+
 }
+
+
+
+let aProductos = [
+    {
+        imagen: '#',
+        nombre: 'Pastel',
+        categoria: 'Dulce',
+        precio: 4999,
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+    },
+    {
+        imagen: '#',
+        nombre: 'Bizcocho',
+        categoria: 'Salado',
+        precio: 4999,
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+    },
+    {
+        imagen: '#',
+        nombre: 'Pastel',
+        categoria: 'Dulce',
+        precio: 4999,
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+    },
+    {
+        imagen: '#',
+        nombre: 'Pastel',
+        categoria: 'Dulce',
+        precio: 4999,
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+    },
+    {
+        imagen: '#',
+        nombre: 'Pastel',
+        categoria: 'Dulce',
+        precio: 4999,
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+    },
+    {
+        imagen: '#',
+        nombre: 'Bizcocho',
+        categoria: 'Salado',
+        precio: 4999,
+        descripcion: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum deserunt.',
+    },
+];
+
+let aDulce = aProductos.filter(producto => producto.categoria === 'Dulce')
+let aSalado = aProductos.filter(producto => producto.categoria === 'Salado')
+
+console.log(aDulce);
+
+console.log(aSalado);
